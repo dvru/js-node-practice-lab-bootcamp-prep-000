f489ab93576b76dea733aa236eb6e71ea95f2581
Damini Varu <damvaru4@gmail.com>
2019-06-19 22:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 var name = "Damini"
 var height = "51"
 
-console.log("Name:")
+console.log('Name:')
 console.log(name)
 
-console.log("Height:")
+console.log('Height:')
 console.log(parseInt(height+1)
